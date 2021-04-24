d8bfff9ace1ce32c8f97060c93ae506d9e89d623
saugatdhimal <dhimalsaugat77@gmail.com>
2021-04-24 15:35:02 +0545

    displaying fetched data, adding pagination to /feed/slug  page, some css 👉✍

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,41 +1,65 @@
+import { useRouter } from "next/router";
+import styles from "../../styles/Feed.module.css";
+
 export const Feed = ({ pageNumber, articles }) => {
-    console.log(pageNumber, articles)
-    return (
-        <>Hello World</>
-    )
-}
+    const router = useRouter()
+  return (
+    <div className='page=container'>
+      <div className={styles.main}>
+        {articles.map((article, index) => (
+          <div key={index} className={styles.post}>
+            <h1 onClick={() => (window.location.href = article.url)}>
+              {article.title}
+            </h1>
+            <p>{article.description}</p>
+            {article.urlToImage && <img src={article.urlToImage} />}
+          </div>
+        ))}
+      </div>
+      <div className={styles.paginator}>
+          <div onClick={() => {
+              if (pageNumber > 1) {
+                  router.push(`/feed/${pageNumber-1}`)
+              }
+          }} className={pageNumber === 1 ? styles.disabled : styles.active}>
+              Previous Page
+          </div>
+      </div>
+    </div>
+  );
+};
 
-export const getServerSideProps = async pageContext => {
-    const pageNumber = pageContext.query.slug;
+export const getServerSideProps = async (pageContext) => {
+  const pageNumber = pageContext.query.slug;
 
-    if (!pageNumber || pageNumber < 1 || pageNumber > 5) {
-        return {
-            props: {
-                articles: [],
-                pageNumber: 1
-            }
-        }
-    };
-
-    const apiResponse = await fetch (
-        `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page=${pageNumber}`,
-        {
-            headers: {
-                Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`
-            }
-        }
-    );
-
-    const apiJson = await apiResponse.json()
-
-    const { articles } = apiJson;
-
+  if (!pageNumber || pageNumber < 1 || pageNumber > 5) {
     return {
-        props: {
-            articles: articles,
-            pageNumber: Number.parseInt(pageNumber)
-        }
+      props: {
+        articles: [],
+        pageNumber: 1,
+      },
+    };
+  }
+
+  const apiResponse = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page=${pageNumber}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+      },
     }
-}
+  );
+
+  const apiJson = await apiResponse.json();
+
+  const { articles } = apiJson;
+
+  return {
+    props: {
+      articles: articles,
+      pageNumber: Number.parseInt(pageNumber),
+    },
+  };
+};
 
 export default Feed;
